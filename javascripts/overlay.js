7c729b8a850c0b6cc0ae0a74920a2a28e81f1b5a
Grant Custer <grantcuster@gmail.com>
2016-01-08 13:14:56 -0500

    better overlays

--- javascripts/overlay.js
+++ javascripts/overlay.js
@@ -9,25 +9,30 @@ $(document).ready(function() {
 
   function generateOverlayHtml(info) {
     var head_title = info.head_title;
+    var type = info.type;
     var title = info.title;
     var content = info.content;
     var links = info.links;
     var color = info.color;
 
     var html = '';
-    html += '<div class="container px2 overlay-color-' + color + '">';
-    html += '<div class="bg-white px2" style="margin: -0.5rem;">';
-    html += '<div class="clearfix py2 lh1">';
-    html += '<div class="head_title chambers left bold">' + head_title + '</div>';
-    html += '<div id="overlay-close" class="right"><img src="resources/images/x.svg" /></div>';
-    html += '</div>';
-    html += '<div class="nav">';
+    html += '<div class="overlay-container container overlay-color-' + color + '">';
+    html += '<div class="overlay-content bg-white px2">';
+    html += '<div class="border-bottom clearfix py2 h5 mb2">'
+    html += '<div class="left bold head_title">' + head_title + '</div>';
+    html += '<div class="right right-nav bold">';
     for (var i=0; i<links.length; i++) {
-      html += links[i] + '&nbsp;';
+      html += links[i];
     }
+    html += '<div class="overlay-close x-box">';
+    html += '<div class="close-line"></div>';
+    html += '<div class="close-line"></div>';
     html += '</div>';
-    html += '<h2>' + info.title + '</h2>';
+    html += '</div>';
+    html += '</div>';
+    html += '<h1>' + info.title + '</h1>';
     html += content;
+    html += '<div class="clearfix border-top mt3 py2 center h5"><span class="overlay-close bold fake-link">Close This</span></div>';
     html += '</div>';
     html += '</div>';
     return html;
@@ -36,6 +41,71 @@ $(document).ready(function() {
   function closeOverlay() {
     $overlay.html('');
     $body.removeClass('overlayed');
+  }
+
+  function attachPrototypeHandlers($overlay) {
+    var $video_holder = $('.overlay-video');
+    $video_holder.addClass('initial');
+
+    $overlay.on('click', '.overlay-video', function() {
+      $video_holder = $('.overlay-video');
+      $video_holder.removeClass('initial');
+      var $video = $video_holder.find('video');
+      var video = $video.get(0);
+      var $progress_holder = $('.progress-bar-holder ');
+      var $progress_indicator = $('.progress-bar-indicator');
+
+      //update HTML5 video current play time
+      $video.on('timeupdate', function() {
+        var currentPos = video.currentTime; //Get currenttime
+        var maxduration = video.duration; //Get video duration
+        var percentage = 100 * currentPos / maxduration; //in %
+        $progress_indicator.css('width', percentage+'%');
+      });
+
+      var timeDrag = false;   /* Drag status */
+      $progress_holder.mousedown(function(e) {
+        timeDrag = true;
+        updatebar(e.pageX);
+        return false;
+      });
+      $(document).mouseup(function(e) {
+        if(timeDrag) {
+          timeDrag = false;
+          updatebar(e.pageX);
+         }
+      });
+      $(document).mousemove(function(e) {
+        if(timeDrag) {
+          updatebar(e.pageX);
+        }
+      });
+
+      //update Progress Bar control
+      var updatebar = function(x) {
+         var maxduration = video.duration; //Video duraiton
+         var position = x - $progress_holder.offset().left; //Click pos
+         var percentage = 100 * position / $progress_holder.width();
+         //Check within range
+         if(percentage > 100) {
+            percentage = 100;
+         }
+         if(percentage < 0) {
+            percentage = 0;
+         }
+         //Update progress bar and video currenttime
+         $progress_indicator.css('width', percentage+'%');
+         video.currentTime = maxduration * percentage / 100;
+      };
+
+      if (!$video_holder.hasClass('playing')) {
+        $video_holder.addClass('playing');
+        video.play();
+      } else {
+        $video_holder.removeClass('playing');
+        video.pause();
+      }
+    });
   }
 
   $('.overlayer').click(function() {
@@ -47,14 +117,36 @@ $(document).ready(function() {
       });
     }
     $body.addClass('overlayed');
-    var attr = $(this).attr('data-overlay')
+    var attr = $(this).attr('data-overlay');
     var info = window.overlay_objects[attr];
     var html = generateOverlayHtml(info);
     $overlay.scrollTop(0);
     $overlay.html(html);
-    $('overlay-close').click(function() {
+
+    attachPrototypeHandlers($overlay);
+
+    $overlay.on('click', '.overlay-close', function() {
       closeOverlay();
     });
+    $overlay.on('click', '.overlay-content', function(e) {
+      e.stopPropagation();
+    });
+
+    // For overlay links within the overlay
+    $overlay.on('click', '.overlayer', function() {
+      var attr = $(this).attr('data-overlay');
+      var info = window.overlay_objects[attr];
+      var html = generateOverlayHtml(info);
+      $overlay.scrollTop(0);
+      $overlay.html(html);
+    });
+
+  });
+
+  $(document).keyup(function(e) {
+    if (e.keyCode == 27) {
+      closeOverlay();
+    }
   });
 
 });
--- javascripts/report_prototype.js
+++ javascripts/report_prototype.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
 
+  var supportsTouch = !!('ontouchstart' in window) || !!(navigator.msMaxTouchPoints);
+
+  // supportsTouch = true;
+  var noTouch = !supportsTouch;
+
   var reports = {};
 
   // FF01 config
   reports.ff01 = {};
+  reports.ff01.type = "report";
   reports.ff01.color = "cyan";
   reports.ff01.number = "FF01";
   reports.ff01.title = "Natural Language Generation";
@@ -13,6 +19,7 @@ $(document).ready(function() {
 
   // FF02 config
   reports.ff02 = {};
+  reports.ff02.type = "report";
   reports.ff02.color = "magenta";
   reports.ff02.number = "FF02";
   reports.ff02.title = "Probabilistic Methods for Realtime Streams";
@@ -20,8 +27,9 @@ $(document).ready(function() {
   reports.ff02.images = ["00.jpg", "01.jpg", "02.jpg", "03.jpg", "04.jpg"];
   reports.ff02.info = "Realtime analysis is a challenge for modern data systems. The probabilistic methods explored in this report offer highly efficient models for extracting value from streams of data as they are generated. This orders-of-magnitude improvement in efficiency enables many new applications and modes of computation.";
 
-  // FF02 config
+  // FF03 config
   reports.ff03 = {};
+  reports.ff03.type = "report";
   reports.ff03.color = "purple";
   reports.ff03.number = "FF03";
   reports.ff03.title = "Deep Learning: Image Analysis";
@@ -41,15 +49,9 @@ $(document).ready(function() {
       html += '<img class="' + img_class + '" src="images/' + report.image_dir + '/' + report.images[i] + '" />'
     }
     html += '</div>';
-    html += '<div class="report-progress col-12 relative" style="margin-top: 1rem">';
-    html += '<div class="col-12" style="height: 0.1rem; background: #efefef">';
-    for (var i=0; i<report.images.length; i++) {
-      var marker_class = 'report-marker left';
-      if (i==0) {
-        marker_class += ' active';
-      }
-      html += '<div class="' + marker_class + '" style="width: 20%; height: 100%;"></div>';
-    }
+    html += '<div class="report-progress display-none col-12 relative" style="margin-top: 1rem">';
+    html += '<div class="col-12 report-marker-holder" style="height: 0.1rem;">';
+    html += '<div class="report-marker" style="height: 100%; width: 0%;"></div';
     html += '</div>';
     return html;
   }
@@ -69,42 +71,69 @@ $(document).ready(function() {
     var $report_images = $html.find('.report-img');
     var $report_markers = $html.find('.report-marker');
     var image_num = $report_images.length;
-    var sect_width = width/image_num;
-    $html.on("mousemove", function(e) {
-      var $this = $(this);
-      var mouse_x = e.pageX - $this.offset().left;
-      for (var i=0; i<image_num; i++) {
-        var multiplier = i + 1;
-        if (mouse_x < sect_width * multiplier) {
-          $report_images.removeClass('active');
-          $report_images.eq(i).addClass('active');
-          $report_markers.removeClass('active');
-          $report_markers.eq(i).addClass('active');
-          break;
-        }
+
+    var flipInterval = false;
+    var interval_pause = false;
+
+    $this.on('mouseenter', function() {
+      if (noTouch) {
+        flipInterval = setInterval(function() {
+          if (interval_pause) {
+            interval_pause = false;
+          } else {
+            var active_image = $this.find('.active');
+            // index starts at one because of spacer image
+            var active_index = active_image.index();
+            if (active_index == image_num) {
+              next_index = 1;
+              interval_pause = true;
+            } else if (active_index == (image_num - 1)) {
+              interval_pause = true;
+              next_index = active_index + 1;
+            } else {
+              next_index = active_index + 1;
+            }
+            $report_images.removeClass('active');
+            $report_images.eq(next_index - 1).addClass('active');
+          }
+        }, 500);
       }
     });
-    // $html.on("mouseleave", function(e) {
-    //   setTimeout(function() {
-    //     $report_images.removeClass('active')
-    //     $report_images.eq(0).addClass('active');
-    //     $report_markers.removeClass('active');
-    //     $report_markers.eq(0).addClass('active');
-    //   },100);
-    // });
+
+    $this.on('mouseleave', function() {
+      if (noTouch) {
+        if (flipInterval) {
+          clearInterval(flipInterval);
+        }
+        $report_images.removeClass('active');
+        $report_images.eq(0).addClass('active');
+      }
+    });
+
+    $this.on('click', function() {
+      if (noTouch) {
+        clearInterval(flipInterval);
+        $report_images.removeClass('active');
+        $report_images.eq(0).addClass('active');
+      }
+    });
+
   });
 
   var prototypes = {};
 
   // Roborealtor
   prototypes.roborealtor = {};
+  prototypes.roborealtor.type = "prototype";
   prototypes.roborealtor.color = "cyan";
   prototypes.roborealtor.number = "FF01";
   prototypes.roborealtor.name = "Roborealtor";
   prototypes.roborealtor.file_name = "roborealtor";
+  prototypes.roborealtor.info = "Inspired by real estate sites, Roborealtor is a natural language generation prototype that takes the attributes for a hypothetical apartment and generates listings for it.";
 
   // CliqueStream
   prototypes.cliquestream = {};
+  prototypes.cliquestream.type = "prototype";
   prototypes.cliquestream.color = "magenta";
   prototypes.cliquestream.number = "FF02";
   prototypes.cliquestream.name = "CliqueStream";
@@ -112,6 +141,7 @@ $(document).ready(function() {
 
   // Fathom
   prototypes.fathom = {};
+  prototypes.fathom.type = "prototype";
   prototypes.fathom.color = "purple";
   prototypes.fathom.number = "FF03";
   prototypes.fathom.name = "Fathom and Pictograph";
@@ -119,14 +149,11 @@ $(document).ready(function() {
 
   function videoModuleHtml(prototype) {
     var html = '';
-    html += '<div class="relative">';
+    html += '<div class="video-inside-holder">';
+    html += '<div class="relative video-inside">';
     html += '<img class="video-backer" src="resources/images/video-back.svg" />';
     html += '<img class="video-screenshot" src="resources/videos/' + prototype.file_name + '.png" />';
     html += '<div class="sp sp-circle ' + prototype.color + '"></div>'
-    html += '</div>';
-    html += '<div class="video-progress col-12 relative" style="margin-top: 1rem">';
-    html += '<div class="col-12" style="height: 0.1rem; background: #efefef">';
-    html += '<div class="progress-bar" style="width: 20%; height: 100%;"></div>';
     html += '</div>';
     html += '</div>';
     return html;
@@ -144,34 +171,49 @@ $(document).ready(function() {
     var $html = $(html);
     $this.html($html);
 
+    var $progress_indicator = $html.find('.progress-bar');
+
     var video_inserted = false;
     var video_loaded = false;
     var video_playing = false;
 
     $html.on("mouseenter", function() {
-      var $this = $(this);
-      video_playing = true;
-      if (video_inserted) {
-        $this.find('video').get(0).play();
-      } else {
-        $this.addClass('loading');
-        var resource = 'resources/videos/' + prototype.file_name + '.mp4';
-        var $video = $('<video src="' + resource + '" loop="true"></video>');
-        $this.append($video);
-        video_inserted = true;
+      if (noTouch) {
+        var $this = $(this);
+        video_playing = true;
+        if (video_inserted) {
+          $this.find('video').get(0).play();
+        } else {
+          $this.addClass('loading');
+          var resource = 'http://www.fastforwardlabs.com/resources/videos/' + prototype.file_name + '.mp4';
+          var $video = $('<video src="' + resource + '" loop="true"></video>');
+          console.log($this);
+          $this.find('.video-inside').append($video);
+          video_inserted = true;
 
-        var video = $video.get(0);
-        $video.on("canplay", function() {
-          $this.removeClass('loading').addClass('loaded');
-          if (video_playing) {
-            video.play();
-          }
-        })
+          // update HTML5 video current play time
+          $video.on('timeupdate', function() {
+            var currentPos = video.currentTime; //Get currenttime
+            var maxduration = video.duration; //Get video duration
+            var percentage = 100 * currentPos / maxduration; //in %
+            $progress_indicator.css('width', percentage+'%');
+          });
+
+          var video = $video.get(0);
+          $video.on("canplay", function() {
+            $this.removeClass('loading').addClass('loaded');
+            if (video_playing) {
+              video.play();
+            }
+          })
+        }
       }
     });
     $html.on("mouseleave", function() {
-      video_playing = false;
-      $this.find('video').get(0).pause();
+      if (noTouch) {
+        video_playing = false;
+        $this.find('video').get(0).pause();
+      }
     });
   });
 
@@ -179,30 +221,59 @@ $(document).ready(function() {
 
   function generateReportOverlayContent(report) {
     var report_obj = reports[report];
-    var content = '<div class="mb2">';
+    var content = '<div class="container narrower">';
     content += '<p>' + report_obj.info + '</p>';
     content += '</div>';
     content += '<div class="report-overlay-images">';
     for (var i=0; i<report_obj.images.length; i++) {
       var image = report_obj.images[i];
-      var html = '<img src="images/' + report_obj.image_dir + '/' + image + '" />';
+      var html = '<img style="margin: 0 auto 1rem; display: block;" src="images/' + report_obj.image_dir + '/' + image + '" />';
       content += html;
     }
     content += '</div>';
     return content;
   }
 
-  var overlay_ender = '<div class="clearfix" style="margin-top: -0.5rem"><div class="nav clearfix mb2"><div class="right"><a href="#">Back to Product</a></div></div>';
+  function generatePrototypeOverlayContent(prototype) {
+    var prototype_obj = prototypes[prototype];
+    var content = '<div class="container narrower">';
+    var resource = 'http://www.fastforwardlabs.com/resources/videos/' + prototype_obj.file_name + '.mp4';
+    content += '<p>' + prototype_obj.info + '</p>';
+    content += '</div>';
+    content += '<div class="video-holder container relative overlay-video" style="max-width: 38rem; margin-bottom: 0.75rem;">';
+    content += '<div class="loaded">'
+    content += '<img class="video-backer" src="resources/images/video-back.svg" />';
+    content += '<video src="' + resource + '" class="col-12" loop="true"></video>';
+    content += '</div>';
+    content += '<div class="status-indicator">';
+    content += '<i class="fa fa-play"></i>';
+    content += '<i class="fa fa-pause"></i>';
+    content += '</div>';
+    content += '</div>';
+    content += '<div class="progress-bar-holder col-12" style="max-width: 38rem; margin: 0 auto;">';
+    content += '<div class="progress-bar-indicator"></div>'
+    content += '</div>';
+    return content;
+  }
 
   window.overlay_objects.ff01 = {};
   var ff01 = window.overlay_objects.ff01;
   ff01.head_title = "FF01";
   ff01.color = "cyan";
   ff01.title = "Natural Language Generation";
-  ff01.links = ['<a class="active" href="#">Report</a>','<a class="" href="#">Prototype</a>'];
+  ff01.links = ['<span class="fake-link active">Report</span>','<span class="overlayer fake-link" data-overlay="roborealtor">Prototype</span>'];
   var content = generateReportOverlayContent("ff01");
   ff01.content = content;
-  ff01.content += overlay_ender;
+
+  window.overlay_objects.roborealtor = {};
+  var roborealtor = window.overlay_objects.roborealtor;
+  roborealtor.head_title = "FF01";
+  roborealtor.type = "prototype";
+  roborealtor.color = "cyan";
+  roborealtor.title = "RoboRealtor"
+  roborealtor.links = ['<span class="fake-link overlayer" data-overlay="ff01">Report</span>','<span class="fake-link active" href="#">Prototype</span>'];
+  var content = generatePrototypeOverlayContent("roborealtor");
+  roborealtor.content = content;
 
   window.overlay_objects.ff02 = {};
   var ff02 = window.overlay_objects.ff02;
@@ -212,16 +283,14 @@ $(document).ready(function() {
   ff02.links = ['<a class="active" href="#">Report</a>','<a class="" href="#">Prototype</a>'];
   var content = generateReportOverlayContent("ff02");
   ff02.content = content;
-  ff02.content += overlay_ender;
 
   window.overlay_objects.ff03 = {};
   var ff03 = window.overlay_objects.ff03;
   ff03.head_title = "FF03";
   ff03.color = "purple";
   ff03.title = "Deep Learning: Image Analysis";
-  ff03.links = ['<a class="active" href="#">Report</a>','<a class="" href="#">Prototype</a>'];
+  ff03.links = ['<span class="active">Report</a>','<span class="">Prototype</span>'];
   var content = generateReportOverlayContent("ff03");
   ff03.content = content;
-  ff03.content += overlay_ender;
 
 });
